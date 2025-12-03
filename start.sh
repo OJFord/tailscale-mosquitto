b2ae6dd77134c94837542c998ee8e3852a1d3bce
Oliver Ford <dev@ojford.com>
2025-12-03 16:16:19 +0000

    Listen only over ts

--- start.sh
+++ start.sh
@@ -8,6 +8,6 @@ tailscale up --auth-key="$TAILSCALE_AUTHKEY" --hostname=mosquitto --advertise-ta
 
 >&2 echo 'Starting mosquitto'
 set -o monitor
-eclipse-mosquitto --port=1883 "$@" &
+eclipse-mosquitto "$@" &
 tailscale serve --service=svc:mqtt-broker 1883
 fg
